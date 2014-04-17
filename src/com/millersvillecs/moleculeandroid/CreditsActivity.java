981f368add2876dbb8f34bca37522c06443cd918
CMahaff <connorsmahaffey@gmail.com>
2014-04-17 12:36:16 -0400

    Added links to contributors and NSF to Credits Screen

--- src/com/millersvillecs/moleculeandroid/CreditsActivity.java
+++ src/com/millersvillecs/moleculeandroid/CreditsActivity.java
@@ -1,9 +1,13 @@
 package com.millersvillecs.moleculeandroid;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.text.Html;
 import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
 import android.widget.TextView;
 
 public class CreditsActivity extends Activity{
@@ -14,5 +18,14 @@ public class CreditsActivity extends Activity{
 		TextView body = (TextView) findViewById(R.id.credits_body_view);
 		Spanned formattedText = Html.fromHtml(getString(R.string.credits_body));
 		body.setText(formattedText);
+	    body.setMovementMethod(LinkMovementMethod.getInstance());
+	}
+	
+	public void onNSF(View view)
+	{
+		String url = "http://www.nsf.gov/awardsearch/showAward?AWD_ID=0968350";
+		Intent i = new Intent(Intent.ACTION_VIEW);
+		i.setData(Uri.parse(url));
+		startActivity(i);
 	}
 }
